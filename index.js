aa748c61907fcdc8b23c4fd923e86b0ebf5a1a64
Nafiu Taiwo <nafiutaiwosolomon@gmail.com>
2024-07-01 03:02:47 +0100

    feat: added major functionality

---
+++ index.js
@@ -0,0 +1,9 @@
+const currentDay = document.getElementById("currentDay");
+const currentTime = document.getElementById("currentTime");
+
+const weekdays = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+
+currentDay.textContent = weekdays[new Date().getUTCDay()]
+
+setInterval(() => currentTime.textContent = new Date().toUTCString(), 1000)
+
